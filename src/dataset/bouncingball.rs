6cc3a19e20bab1748bf8068d36d1c75ac9bcf846
Joseph <Jaghoghov@gmail.com>
2024-10-28 21:25:28 +0000

    feat: added bouncingball dataloader file. reads .npz files and parses as NDarray Array4

--- src/dataset/bouncingball.rs
+++ src/dataset/bouncingball.rs
@@ -1,12 +1,19 @@
 use ndarray::Array4;
 use ndarray_npz::NpzReader;
-use std::fs::File;
+use std::fs::{read_dir, File};
 
 pub fn read_npz_file() {
-    let mut npz =
-        NpzReader::new(File::open("data/00000.npz").expect("should run from project root"))
-            .unwrap();
-    let a1: Array4<f64> = npz.by_index(0).unwrap();
+    let mut file_iterator = read_dir("data/images_train_N_5000_T_100_dim_latent_2_dim_obs_2_resolution_32_state_3_sparsity_0.0_net_cosine_seed_24/").unwrap();
 
-    println!("{:?}", a1);
+    // println!("{:?}", file_iterator.unwrap());
+    let mut index: i32 = 0;
+    while let Some(sequence) = &file_iterator.next() {
+        let seq = sequence.as_ref().unwrap();
+        let mut npz =
+            NpzReader::new(File::open(seq.path()).expect("should run from project root")).unwrap();
+        let a1: Array4<f64> = npz.by_index(0).unwrap();
+        index += 1;
+
+        println!("{index}")
+    }
 }
